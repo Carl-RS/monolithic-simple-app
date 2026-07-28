b3d7267da0971b816fd82762e3b8cb52da5a0d05
Carlos Vega <carlos.vega2718@gmail.com>
2026-07-28 13:33:55 -0500

    Forced GA failure

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -15,3 +15,11 @@ test('Invalid Login', async ({ page }) => {
     await page.getByTestId('login-submit-btn').click();
     await expect(page.getByTestId('error-message')).toBeVisible();
 });
+
+test('Fail GA test', { tag: '@smoke' }, async ({ page }) => {
+    await page.goto("/");
+    await page.getByTestId('username-input').fill('admin');
+    await page.getByTestId('password-input').fill('passwtree');
+    await page.getByTestId('login-submit-btn').click();
+    await expect(page.getByTestId('welcome-message')).toBeVisible();
+});
