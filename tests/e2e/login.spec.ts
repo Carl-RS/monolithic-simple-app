94d262501db53d71a9ed7de5ed6b37017842f055
Carlos Vega <carlos.vega2718@gmail.com>
2026-07-28 21:35:27 -0500

    Removed failing test

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -16,10 +16,10 @@ test('Invalid Login', async ({ page }) => {
     await expect(page.getByTestId('error-message')).toBeVisible();
 });
 
-test('Fail GA test', { tag: '@smoke' }, async ({ page }) => {
-    await page.goto("/");
-    await page.getByTestId('username-input').fill('admin');
-    await page.getByTestId('password-input').fill('passwtree');
-    await page.getByTestId('login-submit-btn').click();
-    await expect(page.getByTestId('welcome-message')).toBeVisible();
-});
+// test('Fail GA test', { tag: '@smoke' }, async ({ page }) => {
+//     await page.goto("/");
+//     await page.getByTestId('username-input').fill('admin');
+//     await page.getByTestId('password-input').fill('passwtree');
+//     await page.getByTestId('login-submit-btn').click();
+//     await expect(page.getByTestId('welcome-message')).toBeVisible();
+// });
